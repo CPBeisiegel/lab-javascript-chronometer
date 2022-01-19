0a0e50d4691130e495f625689acd6265f8b79504
Camila Beisiegel <camila.beisiegel@gmail.com>
2022-01-18 22:09:53 -0300

    class chronometer solved minus DOM functions

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,50 @@
 class Chronometer {
   constructor() {
     // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
-  start(callback) {
-    // ... your code goes here
+  start() {
+    this.intervalId = setInterval(() => {
+      this.currentTime++}, 1000)
+   
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60)
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    // Tentar entender a solução usando padStart
+      /* return String(value).padStart(10,"0") */
+    
+   if(value < 10){
+      return '0' + value 
+    } else{
+      return String(value)
+    }
   }
 
   stop() {
-    // ... your code goes here
+    // The global clearInterval() method cancels a timed,
+    // repeating action which was previously established by a call to setInterval().
+    clearInterval(this.intervalId)
   }
 
   reset() {
-    // ... your code goes here
+   this.currentTime = 0
   }
 
   split() {
-    // ... your code goes here
+    const minutes = this.computeTwoDigitNumber(this.getMinutes());
+    const seconds = this.computeTwoDigitNumber(this.getSeconds());
+
+    return `${minutes}:${seconds}`
   }
 }
 
